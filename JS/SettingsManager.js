d0c7cf670c989110899eca0e55028084d509a990
Max_ <maxb777hk@gmail.com>
2017-10-09 21:21:56 +0300

    settings fixed

--- JS/SettingsManager.js
+++ JS/SettingsManager.js
@@ -13,28 +13,32 @@ function Settings() {
         };
 }
 function SettingsManager() {
-    this.settings = new Settings();
+    this.settings = {};
+    var settingsObj = new Settings();
 
-    setWrappers(this.settings,this);
-    function setWrappers(source, dest, prevProps)
-    {
+    loadSettings();
+
+    setWrappers(settingsObj, this.settings);
+
+    function setWrappers(source, dest, prevProps) {
         prevProps = prevProps || [];
         for (var s in source) {
-            var context = {
-                source: source,
-                name: s.slice(0),
-                prevProps: prevProps,
-            }
-            if(typeof(source[s]) == 'object')
-            {
+            if (typeof(source[s]) == 'object') {
                 // Object.defineProperty(dest, s, {
                 //     //writable:false
                 // });
                 dest[s] = {};
-                setWrappers(source[s],dest[s],prevProps.concat(s));
+                setWrappers(source[s], dest[s], prevProps.concat(s));
+                Object.defineProperty(dest, s, {
+                    writable:false
+                });
             }
-            else
-            {
+            else {
+                var context = {
+                    source: source,
+                    name: s.slice(0),
+                    prevProps: prevProps,
+                }
                 Object.defineProperty(dest, s, {
                     get: function () {
                         return this.source[this.name];
@@ -48,15 +52,21 @@ function SettingsManager() {
             }
         }
     }
+
     var saveSettings = function ()
     {
-        var json = JSON.stringify(this.settings);
+        var json = JSON.stringify(settingsObj);
         localStorage.setItem('settings',json);
     }.bind(this);
-    this.loadSettings = function () {
+
+    var loadSettings = function () {
         var json = localStorage.getItem('settings');
         var savedSettings = JSON.parse(json);
-        $.extend( true,settings,savedSettings );
+        $.extend( true,settingsObj,savedSettings );
     };
 }
 
+var sss = new SettingsManager();
+sss.settings.drawing.strokeColor='white';
+alert(sss.settings.drawing.strokeColor);
+alert(sss.settings.background.gridStep);
